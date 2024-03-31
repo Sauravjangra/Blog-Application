4b96ef28ae298f5477e8d0a898e7781c82461873
Sauravjangra <sauravjangra27@gmail.com>
2024-03-31 12:29:53 +0530

    msg

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true}))
 app.use(bodyParser.urlencoded({ extended: true}))
-app.use('/', Router);
+// app.use('/', Router);
 
 
 
