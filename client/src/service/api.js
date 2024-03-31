55ec35546f1d52eb70521d04ac182d8eba09472e
Sauravjangra <sauravjangra27@gmail.com>
2024-03-31 12:43:49 +0530

    msg

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = '';
+const API_URL = 'https://blog-application-sand.vercel.app';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
