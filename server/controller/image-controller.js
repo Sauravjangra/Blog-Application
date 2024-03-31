db874faef85de7dcfb4b8171b57af3c9fe5e3a47
Sauravjangra <sauravjangra27@gmail.com>
2024-03-31 12:51:00 +0530

    msg

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = '';
+const url = 'https://blog-application-sand.vercel.app';
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
